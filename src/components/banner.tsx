94215c05ffd00cf69ce0ec58f05c8aa158857456
ashelygierisch <julialee.dev0907@gmail.com>
2026-09-09 23:17:05 +0800

    refac: url change

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -12,7 +12,7 @@ const Banner: React.FC = (props) => {
                 >
                     <Link to="/translate">Get started</Link>
                 </span>
-                <a href="https://github.com/ObservedObserver/chatgpt-i18n" className="text-sm font-semibold leading-6 text-gray-50">
+                <a href="https://github.com/ashelygierisch/chatgpt-i18n" className="text-sm font-semibold leading-6 text-gray-50">
                     Learn more <span aria-hidden="true">→</span>
                 </a>
             </div>
